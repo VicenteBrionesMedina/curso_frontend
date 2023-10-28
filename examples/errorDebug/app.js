e4dc40ec003b4a755cad67d24cc8d4dbea1a2719
Vicente Briones <vicentebriomed@gmail.com>
2023-10-27 21:43:50 -0300

    Organize Examples and Create Tree of Asterisks

---
+++ examples/errorDebug/app.js
@@ -0,0 +1,10 @@
+/*Arbol de Asteriscos*/
+function tree(floorNum){
+    for (let i = 0; i < floorNum; i++){
+        let floor = "*";
+        for (let j = 0; i < j; j++){
+            floor = floor + " ";
+        }
+        console.log(floor)
+    }
+}
