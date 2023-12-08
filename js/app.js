bc83de318b7f57c74b0424c7a814781285b44afb
Vicente Briones <vicentebriomed@gmail.com>
2023-12-08 17:31:40 -0300

    Create Inline Events Example

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 /*Comprobación de Datos*/
 
+//Capturar Valor Nobre
 const nicknameInput = document.getElementById("nickname");
 console.log(nicknameInput.nodeType);
-//nicknameInput.value = "Vicente";
+nicknameInput.value = "Vicente";
 console.log(nicknameInput.value)
 
+//Capturar Valor Select
 const inputSize = document.getElementById("size");
 console.log(inputSize.value)
 console.log(inputSize.options[inputSize.selectedIndex].text);
+
+//Eventos
+function test(){
+    console.log("Evento Click")
+}
